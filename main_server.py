c0f22c41b11238efc16d157811d989b88283ded9
Ali Y <d.aliyamini@gmail.com>
2023-05-05 15:11:16 -0400

    adding server and modes.

---
+++ main_server.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, jsonify
+from mode_config import ModeManager
+
+app = Flask(__name__)
+mode_manager = ModeManager()
+
+@app.after_request
+def add_header(response):
+    """Adding header's for robot's response.
+
+    Returns:
+        json: Server's response
+    """
+    response.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
+    return response
+
+
+@app.route("/")
+def index():
+    """A sample page for 0.0.0.0
+
+    Returns:
+        html: An html page. 
+    """
+    return render_template('menu.html', menu=mode_manager.menu_config)
+
+
+@app.route("/run/<mode_name>", methods=['POST'])
+def run(mode_name):
+    # Run different modes.
+    mode_manager.run(mode_name)
+    response = {'message': f'{mode_name} running'}
+    if mode_manager.should_redirect(mode_name):
+        response['redirect'] = True
+    return jsonify(response)
+
+@app.route("/status")
+def status_check():
+    battery_level = 90 # Placeholder for battery status.
+    if mode_manager.is_running():
+        return jsonify({"status": "processing", f"battery_level":"{battery_level}"})
+    else:
+        return jsonify({"status": "idle", f"battery_level":"{battery_level}"})
+
+@app.route("/stop", methods=['POST'])
+def stop():
+    # Turn off every system.
+    return jsonify({'message': "Stopped"})
+
+@app.route("/stop_process", methods=['POST'])
+def stop():
+    # Turn off every system.
+    mode_manager.stop()
+    return jsonify({'message': "Stopped"})
+
+
+app.run(host="0.0.0.0")
