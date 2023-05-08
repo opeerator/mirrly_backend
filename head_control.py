02c01aab5d02cb5decf5f9a282392172557dc3c0
Ali Y <d.aliyamini@gmail.com>
2023-05-07 20:53:31 -0400

    head motor movement controls.

--- head_control.py
+++ head_control.py
@@ -1,3 +1,5 @@
+# sudo apt-get install python3-setuptools
+# sudo python3 setup.py install
 import os
 
 if os.name == 'nt':
@@ -30,7 +32,9 @@ PROTOCOL_VERSION            = 1.0
 # Factory default ID of all DYNAMIXEL is 1
 DXL_ID_1                      = 1 # Yaw
 DXL_ID_2                      = 2 # Pitch
-DXL_ID_5                      = 5 # eye
+DXL_ID_3                      = 3 # Left eye
+DXL_ID_4                      = 4 # Right eye
+DXL_ID_5                      = 5 # Eye
 
 # ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
 DEVICENAME                  = '/dev/ttyUSB0'
@@ -39,95 +43,159 @@ TORQUE_ENABLE               = 1     # Value for enabling the torque
 TORQUE_DISABLE              = 0     # Value for disabling the torque
 DXL_MOVING_STATUS_THRESHOLD = 20    # Dynamixel moving status threshold
 
-index = 0
-dxl_goal_position_1 = [970, 590]         # Goal position for Yaw
-dxl_goal_position_2 = [237, 137]         # Goal position for Pitch
-dxl_goal_position_5 = [919, 720]         # Goal position for eye
+class HeadMotors(object):
+    """This class manager robot's head motion commands"""
+    
+    def __init__(self):
+        self.dxl_yaw_range = [970, 590]         # Goal position for Yaw
+        self.dxl_pitch_range = [237, 137]         # Goal position for Pitch
+        self.dxl_eyes_range = [919, 720]         # Goal position for eye
+        
+        self.moving = False
 
-# Initialize PortHandler instance
-# Set the port path
-# Get methods and members of PortHandlerLinux or PortHandlerWindows
-portHandler = PortHandler(DEVICENAME)
+        # Initialize PortHandler instance
+        # Set the port path
+        # Get methods and members of PortHandlerLinux or PortHandlerWindows
+        self.portHandler = PortHandler(DEVICENAME)
 
-# Initialize PacketHandler instance
-# Set the protocol version
-# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
-packetHandler = PacketHandler(PROTOCOL_VERSION)
+        # Initialize PacketHandler instance
+        # Set the protocol version
+        # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+        self.packetHandler = PacketHandler(PROTOCOL_VERSION)
 
-# Open port
-if portHandler.openPort():
-    print("Succeeded to open the port")
-else:
-    print("Failed to open the port")
-    print("Press any key to terminate...")
-    getch()
-    quit()
-
-
-# Set port baudrate
-if portHandler.setBaudRate(BAUDRATE):
-    print("Succeeded to change the baudrate")
-else:
-    print("Failed to change the baudrate")
-    print("Press any key to terminate...")
-    getch()
-    quit()
-
-# Enable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-else:
-    print("Dynamixel has been successfully connected")
-
-while 1:
-    print("Press any key to continue! (or press ESC to quit!)")
-    if getch() == chr(0x1b):
-        break
-
-    # Write goal position
-    if (MY_DXL == 'XL320'): # XL320 uses 2 byte Position Data, Check the size of data in your DYNAMIXEL's control table
-        # dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID_1, ADDR_GOAL_POSITION, dxl_goal_position_1[index])
-        # dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID_2, ADDR_GOAL_POSITION, dxl_goal_position_2[index])
-        dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID_5, ADDR_GOAL_POSITION, dxl_goal_position_5[index])
-    else:
-        pass
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-
-    while 1:
-        # Read present position
-        if (MY_DXL == 'XL320'): # XL320 uses 2 byte Position Data, Check the size of data in your DYNAMIXEL's control table
-            dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID_5, ADDR_PRESENT_POSITION)
+        # Open port
+        if self.portHandler.openPort():
+            print("Succeeded to open the port")
         else:
-            dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID_5, ADDR_PRESENT_POSITION)
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print("%s" % packetHandler.getRxPacketError(dxl_error))
+            print("Failed to open the port")
+            print("Press any key to terminate...")
+            getch()
+            quit()
 
-        print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID_5, dxl_goal_position_5[index], dxl_present_position))
+        # Set port baudrate
+        if self.portHandler.setBaudRate(BAUDRATE):
+            print("Succeeded to change the baudrate")
+        else:
+            print("Failed to change the baudrate")
+            print("Press any key to terminate...")
+            getch()
+            quit()
 
-        if not abs(dxl_goal_position_5[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
-            break
+        # Enable Dynamixel Torque
+        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_1, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+        dxl_comm_result_2, dxl_error_2 = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_2, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+        dxl_comm_result_3, dxl_error_3 = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_3, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+        dxl_comm_result_4, dxl_error_4 = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_4, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+        dxl_comm_result_5, dxl_error_5 = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_5, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
 
-    # Change goal position
-    if index == 0:
-        index = 1
-    else:
-        index = 0
+        # if dxl_comm_result != COMM_SUCCESS:
+        #     print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        # elif self.dxl_error != 0:
+        #     print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+        # else:
+        #     print("Dynamixel has been successfully connected")
+            
+    def move(self, component, g_pos):
+        # A function to move the robot from server commands
 
+        # Move Component
+        dxl_comm_result, dxl_error = None
+        dxl_comm_result_2, dxl_error_2 = None
+        
+        # Wait for last move
+        while self.moving == True:
+            pass
+        
+        self.moving == True
+        if component == "head_yaw":
+            dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, DXL_ID_1, ADDR_GOAL_POSITION, g_pos)
+        elif component == "head_pitch":
+            dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, DXL_ID_2, ADDR_GOAL_POSITION, g_pos)
+        elif component == "eye_brow":
+            dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, DXL_ID_3, ADDR_GOAL_POSITION, g_pos)
+            dxl_comm_result_2, dxl_error_2 = self.packetHandler.write2ByteTxRx(self.portHandler, DXL_ID_4, ADDR_GOAL_POSITION, g_pos)
+        elif component == "eye_self":
+            dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, DXL_ID_5, ADDR_GOAL_POSITION, g_pos)
+        else:
+            pass
+        self.moving == False
+        
+        
+        if dxl_comm_result != None:
+            if dxl_comm_result != COMM_SUCCESS:
+                print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+                return "success_1"
+            elif dxl_error != 0:
+                print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+                return "failed_1"
+            
+        if dxl_comm_result_2 != None:
+            if dxl_comm_result_2 != COMM_SUCCESS:
+                print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result_2))
+                return "success_2"
+            elif dxl_error_2 != 0:
+                print("%s" % self.packetHandler.getRxPacketError(dxl_error_2))
+                return "failed_2"
 
-# Disable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_5, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
+    def current_pos(self, component):
+        if component == "all":
+            dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_1, ADDR_PRESENT_POSITION)
+            dxl_present_position_2, dxl_comm_result_2, dxl_error_2 = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_2, ADDR_PRESENT_POSITION)
+            dxl_present_position_3, dxl_comm_result_3, dxl_error_3 = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_3, ADDR_PRESENT_POSITION)
+            dxl_present_position_4, dxl_comm_result_4, dxl_error_4 = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_4, ADDR_PRESENT_POSITION)
+            dxl_present_position_5, dxl_comm_result_5, dxl_error_5 = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_5, ADDR_PRESENT_POSITION)
+        elif component == "head_yaw":
+            dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_1, ADDR_PRESENT_POSITION)
+        elif component == "head_pitch":
+            dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_2, ADDR_PRESENT_POSITION)
+        elif component == "eye_brow":
+            dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_3, ADDR_PRESENT_POSITION)
+            dxl_present_position_2, dxl_comm_result_2, dxl_error_2 = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_4, ADDR_PRESENT_POSITION)
+        elif component == "eye_self":
+            dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID_5, ADDR_PRESENT_POSITION)
+        else:
+            pass
+        
+        positions = []
+        if component == "all":
+            positions.append(dxl_present_position)
+            positions.append(dxl_present_position_2)
+            positions.append(dxl_present_position_3)
+            positions.append(dxl_present_position_4)
+            positions.append(dxl_present_position_5)
+        elif component == "eye_brow":
+            positions.append(dxl_present_position)
+            positions.append(dxl_present_position_2)
+        else:
+            positions.append(dxl_present_position)
+            
+        return positions
+            
+    def disable_torque(self, component):
+        if component == "all":
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_1, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+            dxl_comm_result_2, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_2, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+            dxl_comm_result_3, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_3, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+            dxl_comm_result_4, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_4, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+            dxl_comm_result_5, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_5, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
 
-# Close port
-portHandler.closePort()
+        elif component == "head_yaw":
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_1, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+        elif component == "head_pitch":
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_2, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+        elif component == "eye_brow":
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_3, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+            dxl_comm_result_2, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_4, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+        elif component == "eye_self":
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID_5, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+        else:
+            pass
+        
+        # if dxl_comm_result != COMM_SUCCESS:
+        #     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        # elif dxl_error != 0:
+        #     print("%s" % packetHandler.getRxPacketError(dxl_error))
+        
+    def close(self):
+        # Close port
+        self.portHandler.closePort()
--- main_server.py
+++ main_server.py
@@ -3,9 +3,13 @@ import subprocess
 from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, emit
 from mode_config import ModeManager
+from head_control import HeadMotors
+
+mode_manager = ModeManager()
+head_motors = HeadMotors()
+current_move = None
 
 app = Flask(__name__)
-mode_manager = ModeManager()
 app.config['SECRET_KEY'] = 'somesecret'
 socketio = SocketIO(app)
 
@@ -43,6 +47,15 @@ def run(mode_name):
         response['redirect'] = True
     return jsonify(response)
 
+@app.route("/run/<mode_name>", methods=['POST'])
+def run(mode_name):
+    # Run different modes.
+    mode_manager.run(mode_name)
+    response = {'message': f'{mode_name} running'}
+    if mode_manager.should_redirect(mode_name):
+        response['redirect'] = True
+    return jsonify(response)
+
 @app.route("/status")
 def status_check():
     battery_level = 90 # Placeholder for battery status.
@@ -50,6 +63,16 @@ def status_check():
         return jsonify({"status": "processing", f"battery_level":"{battery_level}"})
     else:
         return jsonify({"status": "idle", f"battery_level":"{battery_level}"})
+    
+@app.route("/head/move/<component>/<gpos>", methods=['POST'])
+def move_head_m(component, gpos):
+    res = HeadMotors.move(component, gpos)
+    return jsonify({"status": f"{res}"})
+
+@app.route("/head/getinfo/<component>", methods=['POST'])
+def head_info(component):
+    res = HeadMotors.current_pos(component)
+    return jsonify({f"{component}": f"{res}"})
 
 @app.route("/stop/<mode_name>", methods=['POST'])
 def stop(mode_name):
@@ -59,3 +82,4 @@ def stop(mode_name):
 
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0')
+    head_motors.close()
