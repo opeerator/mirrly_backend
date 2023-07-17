d74da430059fc1341f5e53ddfc75e380051846a1
Ali <d.aliyamini@gmail.com>
2023-07-17 15:47:04 -0400

    Working demo.

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ from head_control import HeadMotors
 from torso_control import TorsoMotors
 
 mode_manager = ModeManager()
-head_motors = HeadMotors()
+head_motors = HeadMotors() # From back 970 means to right / 590 means to left
 torso_motors = TorsoMotors()
 current_move = None
 
@@ -184,8 +184,8 @@ def pitch_roll():
         random_value = random.randint(137, 237)
         random_speed = random.randint(800, 1000)
         head_motors.move("head_pitch", random_value, 1000)
-
-def show_webcam_output(cam_index):
+        
+def show_webcam1(cam_index):
     # Open the webcam
     cap = cv2.VideoCapture(cam_index)
 
@@ -204,12 +204,7 @@ def show_webcam_output(cam_index):
         # Resize the frame
         frame = cv2.resize(frame, (frame_width, frame_height))
 
-        # Display the frame
-        if cam_index == 0:
-            cv2.imshow(f'Right Eye - {cam_index}', frame)
-        if cam_index == 2:
-            cv2.imshow(f'Left Eye - {cam_index}', frame)
-
+        cv2.imshow(f'Right Eye - {cam_index}', frame)
 
         # Exit loop if 'q' key is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -218,6 +213,102 @@ def show_webcam_output(cam_index):
     # Release the webcam and close the window
     cap.release()
     cv2.destroyAllWindows()
+    
+def show_webcam_output(cam_index):
+    # Create a face detector
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Set the desired frame width and height
+    frame_width = 320
+    frame_height = 240
+
+    # Open the webcam
+    cap = cv2.VideoCapture(cam_index)  # Assuming webcam2 is used
+
+    # Set the frame size for the webcam
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+    # Calculate the center of the frame
+    frame_center_x = frame_width // 2
+
+    # Calculate the range of yaw movement
+    yaw_min = 590
+    yaw_max = 970
+
+    # Calculate the scaling factor
+    scale_factor = (yaw_max - yaw_min) / frame_width
+
+    # Initialize the previous face position
+    prev_face_position = None
+    is_moving = False
+
+    while True:
+        # Read the current frame
+        ret, frame = cap.read()
+
+        # Resize the frame
+        frame = cv2.resize(frame, (frame_width, frame_height))
+
+        # Convert the frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the grayscale frame
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        # Initialize the face center variable
+        face_center_x = None
+
+        if len(faces) > 0:
+            # Select a random face from the detected faces
+            selected_face = random.choice(faces)
+
+            # Get the position of the selected face
+            (x, y, w, h) = selected_face
+
+            # Calculate the center of the selected face
+            face_center_x = x + w // 2
+
+            # Draw a rectangle around the face
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Calculate the horizontal movement based on the face position relative to the frame center
+        if face_center_x is not None:
+            horizontal_movement = frame_center_x - face_center_x
+
+            # Calculate the yaw movement based on the scaling factor
+            yaw_movement = (-1 * int(horizontal_movement * scale_factor)) / 7
+            print(f"YawMovement: {yaw_movement}")
+
+            # Perform yaw movement based on the horizontal face movement
+            try:
+                current_yaw = head_motors.current_pos("head_yaw")[0]
+            except:
+                print("dynamixel Problem on yaw")
+                continue
+            new_yaw = current_yaw + yaw_movement
+            yaw_change = abs(current_yaw - new_yaw)
+            print(f"newYaw: {new_yaw}")
+            print(f"change: {yaw_change}")
+            # Check if the new yaw is within the range
+            if yaw_min <= new_yaw <= yaw_max and abs(yaw_change) > 5 and is_moving == False:
+                print("MOVE")
+                is_moving = True
+                head_motors.move("head_yaw", new_yaw, 500)
+                is_moving = False
+
+        # Display the frame
+        cv2.imshow('Webcam', frame)
+
+        # Exit loop if 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+    # Release the webcam and close the window
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 
 
 if __name__ == "__main__":
@@ -227,13 +318,13 @@ if __name__ == "__main__":
         webcam2_index = 2  # Index of the second webcam
 
         # Create threads for each webcam
-        webcam1_thread = threading.Thread(target=show_webcam_output, args=(webcam1_index,))
+        webcam1_thread = threading.Thread(target=show_webcam1, args=(webcam1_index,))
         webcam2_thread = threading.Thread(target=show_webcam_output, args=(webcam2_index,))
 
         # Start the webcam threads
         webcam1_thread.start()
         webcam2_thread.start()
-        
+        """
         print(head_motors.current_pos("eye_brow"))  # Left-Right
         initial_positions()
         time.sleep(1)
@@ -247,12 +338,12 @@ if __name__ == "__main__":
         time.sleep(5)
         head_motors.move("head_pitch", 100)
         play_speech("greeting")
-        
+        """
         while True:
-            eye_brow_idle()
-            eye_roll()
+            # eye_brow_idle()
+            # eye_roll()
             pitch_roll()
-            yaw_roll()
+            # yaw_roll()
             play_speech("random")
 
             time.sleep(0.5)
